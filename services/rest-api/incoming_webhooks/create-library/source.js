bf4ba5d4ced893271de04cbc89c87a6549bfbf80
Steve Thompson <writetome51@gmail.com>
2019-08-14 13:22:29 -0600

    Minor refactoring on webhooks get-user, create-user, and create-library. Added comments reminding me to encrypt password before sending it to the db

--- services/rest-api/incoming_webhooks/create-library/source.js
+++ services/rest-api/incoming_webhooks/create-library/source.js
@@ -1,22 +1,22 @@
 // This function is the webhook's request handler.
 exports = async function(payload, response) {
-  
-  var body = context.functions.execute("getRequestBody", payload);
-  
-  if (invalid(body.secret)) return JSON.stringify({error: "invalid secret"});
-  if (!(body.email) || !(body.password) || !(body.library)) return JSON.stringify(
-    {error:"At least 1 of the required PATCH parameters is missing"}
-  );
-  
-  // new document must have email and password so createLibrary() can verify
-  // user identity.
-  const newLibrary = {email: body.email, password: body.password, library: body.library};
-  var result = await context.functions.execute("createLibrary", newLibrary);
-  return JSON.stringify(result);
-  
-  
-  function invalid(secret){
-    return context.functions.execute("secretInvalid", secret);
-  }
-   
-}
+
+	var body = context.functions.execute("getRequestBody", payload);
+
+	if (invalid(body.secret)) return JSON.stringify({error: "invalid secret"});
+	if (!(body.email) || !(body.password) || !(body.library)) return JSON.stringify(
+		{error: "At least 1 of the required PATCH parameters is missing"}
+	);
+
+	// encrypt body.password here before passing it to createLibrary()...
+
+	if (!(body.library.images)) body.library.images = [];
+	var result = await context.functions.execute("createLibrary", body);
+	return JSON.stringify(result);
+
+
+	function invalid(secret) {
+		return context.functions.execute("secretInvalid", secret);
+	}
+
+};
--- services/rest-api/incoming_webhooks/create-user/source.js
+++ services/rest-api/incoming_webhooks/create-user/source.js
@@ -1,19 +1,21 @@
 // This function is the webhook's request handler.
 exports = async function(payload, response) {
-  var body = context.functions.execute("getRequestBody", payload);
-  
-  if (invalid(body.secret)) return JSON.stringify({error: {message:"invalid secret"}});
-  if (!(body.email) || !(body.password)) return JSON.stringify(
-    {error: {message: "At least 1 of the required POST parameters is missing"}}
-  );
-  
-  const newUser = {email: body.email, password: body.password, libraries:[]};
-  var result = await context.functions.execute("createUser", newUser);
-  return JSON.stringify(result);
-  
-  
-  function invalid(secret){
-    return context.functions.execute("secretInvalid", secret);
-  }
-  
+	var body = context.functions.execute("getRequestBody", payload);
+
+	if (invalid(body.secret)) return JSON.stringify({error: {message: "invalid secret"}});
+	if (!(body.email) || !(body.password)) return JSON.stringify(
+		{error: {message: "At least 1 of the required POST parameters is missing"}}
+	);
+
+	// encrypt body.password here before passing it to createUser()...
+
+	body['libraries'] = [];
+	var result = await context.functions.execute("createUser", body);
+	return JSON.stringify(result);
+
+
+	function invalid(secret) {
+		return context.functions.execute("secretInvalid", secret);
+	}
+
 };

b6980dbf7f54694702bbaf9a69e3cb2099fa5bae
Steve Thompson <writetome51@gmail.com>
2019-08-23 18:10:58 -0600

    Refactored changePassword() so it now makes sure requested user exists and the password is correct before it attempts to change the password

--- functions/changePassword/source.js
+++ functions/changePassword/source.js
@@ -1,19 +1,22 @@
 // Returns:  {success: true} | {error: {message: string}} | {error: any}
 
-exports = async function(user) {
-	// user = {email:string, password:string, newPassword:string}.
+exports = async function(properties) {
+	// properties = {email:string, password:string, newPassword:string}.
 
-	var result = await updateOne(user);
+	var user =  context.functions.execute("getUser", properties.email, properties.password);
+	if (user.error) return user;
+
+	var result = await updateOne(user, properties.newPassword);
 	return context.functions.execute("getMessageFromResult", result);
 
 
-	async function updateOne(user) {
+	async function updateOne(user, newPassword) {
 		var users = context.functions.execute("getUsersCollection");
 
 		try {
 			var result = await users.updateOne(
 				{email: user.email, password: user.password},
-				{$set: {password: user.newPassword}}
+				{$set: {password: newPassword}}
 			);
 		} catch (e) {
 			return {error: e};
---
+++ services/rest-api/incoming_webhooks/delete-user/source.js
@@ -0,0 +1,4 @@
+exports = async function(payload, response) {
+
+	// return JSON.stringify(result);
+};

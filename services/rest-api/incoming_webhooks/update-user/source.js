2e051ae321ab8907401c7bd4505841226160a1c5
Steve Thompson <writetome51@gmail.com>
2019-08-26 12:14:28 -0600

    created webhook update-user

---
+++ services/rest-api/incoming_webhooks/update-user/source.js
@@ -0,0 +1,13 @@
+exports = async function(payload) {
+	var props = context.functions.execute(
+		"getPropertiesPreppedForQuerying", payload, ['secret', 'email', 'password', 'propToUpdate', 'newValue']
+	);
+	if (props.error) return JSON.stringify(props);
+
+	// Remember, 'propToUpdate' can contain dot-notation.
+	var result = await context.functions.execute(
+		"updateProperty", props, props.propToUpdate, prop.newValue
+	);
+	result = context.functions.execute("getMessageFromResult", result, 'update');
+	return JSON.stringify(result);
+};

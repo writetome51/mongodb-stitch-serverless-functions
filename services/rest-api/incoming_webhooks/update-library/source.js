ae98e96aab96bef8163720d73d4e9809fa67bf53
Steve Thompson <writetome51@gmail.com>
2019-08-25 21:53:28 -0600

    webhook update-library now returns {success: true} if successful

--- services/rest-api/incoming_webhooks/update-library/source.js
+++ services/rest-api/incoming_webhooks/update-library/source.js
@@ -8,5 +8,6 @@ exports = async function(payload) {
 	var result = await context.functions.execute(
 		"updateProperty", props, ('libraries.' + props.libraryName), props.library
 	);
+	result = context.functions.execute("getMessageFromResult", result, 'update');
 	return JSON.stringify(result);
 };

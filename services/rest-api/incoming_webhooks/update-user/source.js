1a31f29a4a2e2eb8c18eeeccf2405bea1276ba32
Steve Thompson <writetome51@gmail.com>
2019-08-26 12:16:11 -0600

    minor change

--- services/rest-api/incoming_webhooks/update-user/source.js
+++ services/rest-api/incoming_webhooks/update-user/source.js
@@ -6,7 +6,7 @@ exports = async function(payload) {
 
 	// Remember, 'propToUpdate' can contain dot-notation.
 	var result = await context.functions.execute(
-		"updateProperty", props, props.propToUpdate, prop.newValue
+		"updateProperty", props, props.propToUpdate, props.newValue
 	);
 	result = context.functions.execute("getMessageFromResult", result, 'update');
 	return JSON.stringify(result);

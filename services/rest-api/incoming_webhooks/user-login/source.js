cd6ce82c39426019290b4e2f430273f718608502
Steve Thompson <writetome51@gmail.com>
2019-08-28 14:22:30 -0600

    Modified webhook user-login so you cannot log in again if already logged in

--- services/rest-api/incoming_webhooks/user-login/source.js
+++ services/rest-api/incoming_webhooks/user-login/source.js
@@ -8,7 +8,7 @@ exports = async function(payload) {
 
 	try {
 		var result = await users.updateOne(
-			{email: user.email, password: user.password},
+			{email: user.email, password: user.password, loggedIn: false},
 			{
 				$currentDate: {lastLoggedIn: true}, // sets 'lastLoggedIn' to current date-time.
 				$set: {loggedIn: true}

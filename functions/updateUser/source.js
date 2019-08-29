81a2c6f67f9b17c65d04ccb57921b1aeda1eeb55
Steve Thompson <writetome51@gmail.com>
2019-08-29 17:40:23 -0600

    minor bug fix

--- functions/updateUser/source.js
+++ functions/updateUser/source.js
@@ -1,14 +1,24 @@
 exports = async function(user, uniqueSearchCriteria, updatingObject) {
 	var defaultSearchCriteria = {email: user.email, password: user.password, loggedIn: true};
-	modifyObject(defaultSearchCriteria, uniqueSearchCriteria);
+	var searchCriteria = mergeObjects(defaultSearchCriteria, uniqueSearchCriteria);
 
 	var users = context.functions.execute("getUsersCollection");
-	var result = await users.updateOne(defaultSearchCriteria, updatingObject);
+	var result = await users.updateOne(searchCriteria, updatingObject);
 
 	return context.functions.execute("getMessageFromResult", result, 'update');
 
 
-	function modifyObject(objectToModify, overwritingObject) {
-		Object.assign(objectToModify, overwritingObject);
+	function mergeObjects(){
+ 		var res = {};
+ 		for(var i = 0;i<arguments.length;i++){
+			for(x in arguments[i]){
+				res[x] = arguments[i][x];
+			}
+ 		}
+ 		return res;
 	}
+
 };
+
+
+

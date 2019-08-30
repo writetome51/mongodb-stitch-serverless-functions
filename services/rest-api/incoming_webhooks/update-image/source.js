eaaec82a5919240a187868551d2ff372f449c534
Steve Thompson <writetome51@gmail.com>
2019-08-30 15:22:19 -0600

    Created function updateAndReturnImage(). Refactored webhook update-image to use that function

--- services/rest-api/incoming_webhooks/update-image/source.js
+++ services/rest-api/incoming_webhooks/update-image/source.js
@@ -1,21 +1,13 @@
 exports = async function(payload) {
+	return await context.functions.execute("processRequest",
+		payload,
+		['secret', 'email', 'password', 'libraryName', 'imageIndex', 'newValue'],
 
-	var props = context.functions.execute("getPropertiesPreppedForQuerying",
-		payload, ['secret', 'email', 'password', 'libraryName', 'imageIndex', 'newValue']
+		async (props) => {
+			props.imageIndex += ''; // must be a string.
+			if (!(props.newValue.src)) throw new Error("The image must have a 'src' property");
+
+			return await context.functions.execute("updateAndReturnImage", props);
+		}
 	);
-	if (props.error) return JSON.stringify(props);
-
-	props.imageIndex += ''; // must be a string.
-	if (!(props.newValue.src)) return JSON.stringify(
-		{error: {message: "The image must have a 'src' property"}}
-	);
-
-	var result = await context.functions.execute("updateProperty",
-		props, (`libraries.${props.libraryName}.${props.imageIndex}`), props.newValue
-	);
-
-	result = context.functions.execute("getMessageFromResult", result, 'update');
-	if (result.success) result = await context.functions.execute("getImage", props);
-
-	return JSON.stringify(result);
 };

3f463fd35b143abf1131e0d998adfc4186f300e4
Steve Thompson <writetome51@gmail.com>
2019-08-31 16:46:36 -0600

    Updated webhook update-email so that it uses processRequest()

--- functions/updateAndReturnUser/source.js
+++ functions/updateAndReturnUser/source.js
@@ -6,6 +6,8 @@ exports = async function(user, uniqueSearchCriteria, updatingObject) {
 
 	if (result.success) {
 		if (user.newPassword) user.password = user.newPassword; // Must be updated to get user.
+		if (user.newEmail) user.email = user.newEmail; // Must be updated to get user.
+
 		result = await context.functions.execute("getUser",
 			user.email, user.password
 		);
--- services/rest-api/incoming_webhooks/update-email/source.js
+++ services/rest-api/incoming_webhooks/update-email/source.js
@@ -1,26 +1,12 @@
-// The submitted 'newValue' must be an object.
-// It can contain as many key:value pairs to update as you want.
-
 exports = async function(payload) {
-	var user = context.functions.execute(
-		"getPropertiesPreppedForQuerying", payload, ['newEmail']
+	return await context.functions.execute("processRequest",
+		payload,
+		['newEmail'],
+
+		async (props) => {
+			return await context.functions.execute("updateAndReturnUser",
+				props, {}, {$set: {"email": props.newEmail}}
+			);
+		}
 	);
-	if (user.error) return JSON.stringify(user);
-
-	var users = context.functions.execute("getUsersCollection");
-
-	try {
-		var result = await users.updateOne(
-			{email: user.email, password: user.password, loggedIn: true},
-			{$set: {email: user.newEmail}}
-		);
-	} catch (e) {
-		return {error: e};
-	}
-	result = context.functions.execute("getMessageFromResult", result, 'update');
-	if (result.success) result = await context.functions.execute("getUser",
-		user.newEmail, user.password
-	);
-
-	return JSON.stringify(result);
 };

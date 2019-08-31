f85ea203bb08c1d037e12846ebc5c3eb57e187f8
Steve Thompson <writetome51@gmail.com>
2019-08-31 17:01:41 -0600

    Minor change to updateAndReturnUser()

--- functions/updateAndReturnUser/source.js
+++ functions/updateAndReturnUser/source.js
@@ -1,8 +1,12 @@
 exports = async function(user, uniqueSearchCriteria, updatingObject) {
-
-	var result = await context.functions.execute("updateUser",
-		user, uniqueSearchCriteria, updatingObject
-	);
+	try{
+		var result = await context.functions.execute("updateUser",
+			user, uniqueSearchCriteria, updatingObject
+		);
+	}
+	catch (e) {
+		throw new Error(e.message);
+	}
 
 	if (result.success) {
 		if (user.newPassword) user.password = user.newPassword; // Must be updated to get user.

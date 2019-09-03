858e328f5d3e9a10d100a60d314de0faf11d158e
Steve Thompson <writetome51@gmail.com>
2019-09-03 12:55:31 -0600

    Minor change to create-library webhook

--- services/rest-api/incoming_webhooks/create-library/source.js
+++ services/rest-api/incoming_webhooks/create-library/source.js
@@ -10,6 +10,8 @@ exports = async function(payload) {
 			props['_user_id'] = user._id; // cannot ever change, or relation to user document is lost.
 			props['_id'] = BSON.ObjectId().toString(); // unique property, cannot ever change.
 
+			removeAnyPropertiesNotRequired(props, ['_id', '_user_id', 'name', 'images']);
+
 			let result = await context.functions.execute("createLibrary", props);
 
 			if (result.success) return await context.functions.execute("getImageLibrary",
@@ -18,4 +20,23 @@ exports = async function(payload) {
 			else throw new Error(result);
 		}
 	);
+
+
+	function removeAnyPropertiesNotRequired(properties, requiredProperties) {
+		for (let prop in properties) {
+			if (!(found(prop, requiredProperties))) delete properties[prop];
+		}
+
+
+		// Necessary because Array.includes() is not supported in MongoDB Stitch.
+
+		function found(value, array) {
+			for (var i = 0; i < array.length; ++i) {
+				if (array[i] === value) return true;
+			}
+			return false;
+		}
+	}
+
+
 };

c97e36ce07b53f4c3e18afaa4f65957fe8067f2b
Steve Thompson <writetome51@gmail.com>
2019-10-21 20:18:12 -0600

    Removed one function call from the call stack of webhook "test-update-user". Now it calls updateAndReturnUser() instead of updatePasswordAndReturnUser()

--- services/rest-api/incoming_webhooks/test-update-password/source.js
+++ services/rest-api/incoming_webhooks/test-update-password/source.js
@@ -6,7 +6,18 @@ exports = async function(payload) {
 		async (props) => {
 			props = context.functions.execute("getPropertiesAfterComparingOldAndNewPasswords", props);
 
+			return await context.functions.execute("updateAndReturnUser",
+				props,
+				{
+					email: props.email,
+					password: props.password
+				},
+				{$set: {"password": props.newPassword}}
+			);
+
+			/********
 			return await context.functions.execute("updatePasswordAndReturnUser", props);
+			 ********/
 		}
 	);
 

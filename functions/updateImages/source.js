0cc33b486932db1df4323024498d413ccc4af66f
Steve Thompson <writetome51@gmail.com>
2020-02-15 22:14:05 -0700

    minor change

--- functions/updateImages/source.js
+++ functions/updateImages/source.js
@@ -1,16 +1,14 @@
 // imagesToChange: ImageToChange[]
 // 	  ImageToChange = {
 // 	       name: name of image being updated;
-//         changes: object // The properties in 'changes' can contain dot-notation.
+//         changes: object
+// 		   (The properties in 'changes' can contain dot-notation)
 // 	  }
 //
 
 exports = async function(_user_id, imagesToChange) {
 
 	for (let imageToChange of imagesToChange) {
-		//temp:
-		return {"hello":true};
-
 
 		await context.functions.execute("updateImage",
 			_user_id, imageToChange.name, imageToChange.changes

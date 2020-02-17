99826d632cce3ac6ca473ff49d16a8b4a3fdc02b
Steve Thompson <writetome51@gmail.com>
2020-02-16 18:58:38 -0700

    minor refactoring

--- functions/addImages/source.js
+++ functions/addImages/source.js
@@ -1,33 +1,13 @@
 exports = async function(props) {
 	// Make sure user exists and is logged in before continuing:
-	var user = await getUser(props.sessionID);
-	var imageDocs = getNewImageDocuments(props.images, user._id);
+	var user = await exec("getUser", props.sessionID);
+	var imageDocs = exec("getNewImageDocuments", props.images, user._id);
 
-	var images = getImagesCollection();
+	var images = exec("getImagesCollection");
 	var result = await images.insertMany(imageDocs);
 
-	return getMessageFromResult(result, imageDocs.length);
+	return exec("getMessageFromInsertResult", result, imageDocs.length);
 };
-
-
-async function getUser(sessionID) {
-	return await exec("getUser", sessionID);
-}
-
-
-function getNewImageDocuments(images, user_id) {
-	return exec("getNewImageDocuments", images, user_id);
-}
-
-
-function getImagesCollection() {
-	return exec("getImagesCollection");
-}
-
-
-function getMessageFromResult(result, intendedNumberCreated) {
-	return exec("getMessageFromInsertResult", result, intendedNumberCreated);
-}
 
 
 function exec(funcName, ...args) {

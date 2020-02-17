fcfed968035fd874eb07222dd65cf025d85d4660
Steve Thompson <writetome51@gmail.com>
2020-02-17 00:51:35 -0700

    new commit

--- functions/getAllUserImages/source.js
+++ functions/getAllUserImages/source.js
@@ -1,20 +1,31 @@
 exports = async function(_user_id, batchSize, batchNumber) {
 	try {
-		var images = await __getAllUserImages();
+		var images = await __getBatchOfImages();
 	} catch (e) {
 		throw new Error(e.message);
 	}
 	return images;
 
 
-	async function __getAllUserImages() {
-		var imagesCollection = context.functions.execute("getImagesCollection");
-
-		let docs = await imagesCollection.find({_user_id}).sort({_id: 1}).toArray();
+	async function __getBatchOfImages() {
+		let docs = getAllImages();
 		let _ids = get_ids(docs);
 		let startIndex = (batchNumber - 1) * batchSize;
-		return await imagesCollection.find({_user_id, _id: {$gte: _ids[startIndex]}})
-			.limit(batchSize).toArray();
+		let endIndex = startIndex + batchSize - 1;
+		let searchCriteria = {
+			_user_id,
+			_id: {$gte: _ids[startIndex], $lte: _ids[endIndex]}
+		};
+		if (_ids[endIndex] === undefined) delete searchCriteria['_id']['$lte'];
+		if ((startIndex + 1) > _ids.length) throw new Error(`Batch does not exist`);
+
+		return await imagesCollection.find(searchCriteria).toArray();
+	}
+
+
+	async function getAllImages(){
+		var imagesCollection = context.functions.execute("getImagesCollection");
+		return await imagesCollection.find({_user_id}).sort({_id: 1}).toArray();
 	}
 
 
@@ -25,54 +36,5 @@ exports = async function(_user_id, batchSize, batchNumber) {
 		return _ids;
 	}
 
-
-	function sortByName(images) {
-		images.sort((a, b) => {
-			let aName = a['name'];
-			let bName = b['name'];
-
-			// decides if a or b comes first.
-			return String(aName).localeCompare(String(bName), 'en', {caseFirst: 'upper'});
-		});
-	}
-
-
-	/********************
-
-	function getBatch(batchNumber, batchSize, arr) {
-		let start = (batchNumber - 1) * batchSize;
-		let end = start + batchSize - 1;
-		return _arraySlice(start, end, arr);
-	}
-
-
-	function _arraySlice(start, end, arr) {
-		let len = arr.length;
-		let range = [];
-
-		start = idx(len, start);
-		end = idx(len, end, len);
-
-		while (start < end) {
-			range.push(arr[start++]);
-		}
-		return range;
-
-
-		function idx(len, pos, end = undefined) {
-			if (pos == null) {
-				pos = end || 0;
-			} else if (pos < 0) {
-				pos = Math.max(len + pos, 0);
-			} else {
-				pos = Math.min(pos, len);
-			}
-
-			return pos;
-		}
-
-	}
-
-	 ***********************/
 
 };

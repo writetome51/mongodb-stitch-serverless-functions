88011fadd88a1a3148ba712946c044e9c5fb2d55
Steve Thompson <writetome51@gmail.com>
2020-02-17 01:56:53 -0700

    Improved efficiency for webhook "get-all-images". Should be faster

--- functions/getAllUserImages/source.js
+++ functions/getAllUserImages/source.js
@@ -1,39 +1,60 @@
 exports = async function(_user_id, batchSize, batchNumber) {
 	try {
-		var images = await __getBatchOfImages();
+		var images = await getBatchOfImages();
 	} catch (e) {
 		throw new Error(e.message);
 	}
 	return images;
 
 
-	async function __getBatchOfImages() {
-		let docs = getAllImages();
-		let _ids = get_ids(docs);
-		let startIndex = (batchNumber - 1) * batchSize;
-		let endIndex = startIndex + batchSize - 1;
-		let searchCriteria = {
-			_user_id,
-			_id: {$gte: _ids[startIndex], $lte: _ids[endIndex]}
-		};
-		if (_ids[endIndex] === undefined) delete searchCriteria['_id']['$lte'];
-		if ((startIndex + 1) > _ids.length) throw new Error(`Batch does not exist`);
+	async function getBatchOfImages() {
+		let images = await getAllImagesSorted(_user_id);
 
-		return await imagesCollection.find(searchCriteria).toArray();
+		let [startIndex, endIndex] = get_startIndex_endIndex(batchSize, batchNumber);
+		if ((startIndex + 1) > images.length) throw new Error(`Batch does not exist`);
+
+		return _arraySlice(startIndex, endIndex, images);
 	}
 
 
-	async function getAllImages(){
+	async function getAllImagesSorted(_user_id) {
 		var imagesCollection = context.functions.execute("getImagesCollection");
 		return await imagesCollection.find({_user_id}).sort({_id: 1}).toArray();
 	}
 
 
-	function get_ids(array) {
-		for (var i = 0, _ids = []; i < array.length; ++i) {
-			_ids[i] = array[i]['_id'];
+	function get_startIndex_endIndex(batchSize, batchNumber) {
+		let startIndex = (batchNumber - 1) * batchSize;
+		let endIndex = startIndex + batchSize - 1;
+		return [startIndex, endIndex];
+	}
+
+
+	function _arraySlice(start, end, arr) {
+		let len = arr.length;
+		let range = [];
+
+		start = idx(len, start);
+		end = idx(len, end, len);
+
+		while (start < end) {
+			range.push(arr[start++]);
 		}
-		return _ids;
+		return range;
+
+
+		function idx(len, pos, end = undefined) {
+			if (pos == null) {
+				pos = end || 0;
+			} else if (pos < 0) {
+				pos = Math.max(len + pos, 0);
+			} else {
+				pos = Math.min(pos, len);
+			}
+
+			return pos;
+		}
+
 	}
 
 

9cca3078e60be5b05746ecc1ba177bfa6632c9e4
Steve Thompson <writetome51@gmail.com>
2020-02-19 00:48:50 -0700

    minor refactoring

--- functions/getLibraryImagesBatch/source.js
+++ functions/getLibraryImagesBatch/source.js
@@ -1,35 +1,38 @@
 exports = async function(_image_ids, batchSize, batchNumber) {
 
 	return await context.functions.execute("getBatchOfImages",
-
-		async function() {
-			let imagesCollection = context.functions.execute("getImagesCollection");
-			let unsortedImages = await imagesCollection.find({_id: {$in: _image_ids}}).toArray();
-			return getSorted(unsortedImages);
-		},
-
+		getImagesInProperOrder,
 		batchSize,
 		batchNumber
 	);
 
 
-	function getSorted(unsortedImages){
-		// The array returned must be in same order as _image_ids.
-		let sorted = [];
-		for (let i = 0; i < _image_ids.length; ++i) {
+	async function getImagesInProperOrder() {
+		let imagesCollection = context.functions.execute("getImagesCollection");
+		let unorderedImages = await imagesCollection.find({_id: {$in: _image_ids}}).toArray();
+		return getOrdered(unorderedImages);
 
-			for (let ii = 0; ii < unsortedImages.length; ++ii) {
 
-				if (unsortedImages[ii]['_id'] === _image_ids[i]) {
-					sorted[i] = unsortedImages[ii];
-					unsortedImages.splice(ii, 1); // removes that item.
-					break;
+		function getOrdered(unorderedImages) {
+			// The array returned must be in same order as _image_ids.
+			let ordered = [];
+			for (let i = 0; i < _image_ids.length; ++i) {
+
+				for (let ii = 0; ii < unorderedImages.length; ++ii) {
+
+					if (unorderedImages[ii]['_id'] === _image_ids[i]) {
+						ordered[i] = unorderedImages[ii];
+						unorderedImages.splice(ii, 1); // removes that item.
+						break;
+					}
+
 				}
 
 			}
-
+			return ordered;
 		}
-		return sorted;
+
 	}
+
 
 };

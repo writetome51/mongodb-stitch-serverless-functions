70881177731fd9df8b04680c0b5afde1b805e77f
Steve Thompson <writetome51@gmail.com>
2020-02-19 00:39:41 -0700

    minor refactoring

--- functions/getLibraryImagesBatch/source.js
+++ functions/getLibraryImagesBatch/source.js
@@ -4,16 +4,32 @@ exports = async function(_image_ids, batchSize, batchNumber) {
 
 		async function() {
 			let imagesCollection = context.functions.execute("getImagesCollection");
-			let images = await imagesCollection.find({_id: {$in: _image_ids}}).toArray();
-
-			// The array returned must be in same order as _image_ids.
-			return _image_ids.map(
-				(_id) => images.find((image) => image._id === _id)
-			);
+			let unsortedImages = await imagesCollection.find({_id: {$in: _image_ids}}).toArray();
+			return getSorted(unsortedImages);
 		},
 
 		batchSize,
 		batchNumber
 	);
+
+
+	function getSorted(unsortedImages){
+		// The array returned must be in same order as _image_ids.
+		let sorted = [];
+		for (let i = 0; i < _image_ids.length; ++i) {
+
+			for (let ii = 0; ii < unsortedImages.length; ++ii) {
+
+				if (unsortedImages[ii]['_id'] === _image_ids[i]) {
+					sorted[i] = unsortedImages[ii];
+					unsortedImages.splice(ii, 1); // removes that item.
+					break;
+				}
+
+			}
+
+		}
+		return sorted;
+	}
 
 };

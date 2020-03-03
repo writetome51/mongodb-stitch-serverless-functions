cf2918e1b01468f6363d4ebf259712e55de2299c
Steve Thompson <writetome51@gmail.com>
2020-03-02 21:02:55 -0700

    Due to MongoDB returning JSON that stores numbers as {"$doubleInt": "1"}, we once again converting JSON to a string using JSON.stringify() before returning it

--- functions/processRequest/source.js
+++ functions/processRequest/source.js
@@ -13,17 +13,5 @@ exports = async function(props, requiredProps, uniqueCode) {
 		result = {error: {message: e.message}};
 	}
 
-	// We convert it to string before converting it back to an object because
-	// it solves a problem in how MongoDB returns numbers and dates in its JSON.
-	// For example, in a document, if you set a property 'dataTotal' to a value
-	// of 2, the returned JSON will store 'dataTotal' like this:
-	//
-	//  "dataTotal": {"$numberInt": "2"}
-	//
-	// Calling JSON.stringify() on the object will convert the value {"$numberInt": "2"}
-	// to 2, which is all we need.
-
-	result = JSON.stringify(result);
-
-	return JSON.parse(result);
+	return JSON.stringify(result);
 };

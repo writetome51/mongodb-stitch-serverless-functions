bb3fcd9885aaa5416f8b312a9e129f6216dbf667
Steve Thompson <writetome51@gmail.com>
2020-12-21 17:38:19 -0700

    Added functions getUser() and updatePassword()

---
+++ functions/updatePassword/source.js
@@ -0,0 +1,22 @@
+exports = async function({email, password, newPassword, sessionID}) {
+	let params = {email, password, newPassword, sessionID};
+
+	try {
+		params = context.functions.execute("ifHasPasswordAndSecurityQuestionAnswer_getHashed", params);
+
+		params = context.functions.execute("getPropertiesAfterComparingOldAndNewPasswords", params);
+
+		return await context.functions.execute("updateAndReturnUserAlreadyLoggedIn",
+			params.sessionID,
+			{
+				email: params.email,
+				password: params.password
+			},
+			{$set: {"password": params.newPassword}}
+		);
+	}
+	catch (error) {
+		return {error};
+	}
+
+};
--- functions/userExists/source.js
+++ functions/userExists/source.js
@@ -1,4 +1,4 @@
-exports = async (email) => {
+exports = async ({email}) => {
 	try {
 		var user = await context.functions.execute("getUserByEmail", email);
 	} catch (e) {

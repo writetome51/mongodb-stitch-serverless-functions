465e3391eb969ef7d936400a1c4a3d4545b286f6
Steve Thompson <writetome51@gmail.com>
2020-12-22 17:42:18 -0700

    minor change

--- functions/__getLibrary/source.js
+++ functions/__getLibrary/source.js
@@ -1,4 +1,4 @@
-exports = async function __getLibrary(_user_id, name) {
+exports = async function(_user_id, name) {
 	var libraries = context.functions.execute("getLibrariesCollection");
 	var library = await libraries.findOne({_user_id, name});
 

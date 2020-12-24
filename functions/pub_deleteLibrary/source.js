82d374938c155d6d5317b77428bf93b2f7543b64
Steve Thompson <writetome51@gmail.com>
2020-12-24 10:59:11 -0700

    minor change

--- functions/pub_deleteLibrary/source.js
+++ functions/pub_deleteLibrary/source.js
@@ -1,17 +1,18 @@
 exports = async function({name, sessionID}) {
-	let props = arguments[0];
-	try {
-		var user = await context.functions.execute("getUser", props);
+	return await exec("handlePublicFunction",
+		arguments[0],
 
-		var result = await __deleteLibrary(props.name, user._id);
-		return context.functions.execute("getMessageFromUpdateOrDeleteResult", result, 'delete');
-	} catch (error) {
-		return {error};
-	}
+		async (props) => {
+			var user = await exec("getUser", props);
+
+			var result = await __deleteLibrary(props.name, user._id);
+			return exec("getMessageFromUpdateOrDeleteResult", result, 'delete');
+		}
+	);
 
 
 	async function __deleteLibrary(name, _user_id) {
-		var libraries = context.functions.execute("getLibrariesCollection");
+		var libraries = exec("getLibrariesCollection");
 		return await libraries.deleteOne({_user_id, name});
 	}
 

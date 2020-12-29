0ebf7d2db8fe726c5a2131f1937c17373ae8267b
Steve Thompson <writetome51@gmail.com>
2020-12-29 11:30:05 -0700

    Fixed minor bug

--- functions/getUser/source.js
+++ functions/getUser/source.js
@@ -5,4 +5,10 @@ exports = async function({sessionID}) {
 	// If user wasn't found the sessionID was invalid, meaning user's not logged in.
 	if (!(user) || !(user.loggedIn)) throw new Error("You're not logged in. Log in first");
 	return user;
+
+
+	function exec(funcName, ...args) {
+		return context.functions.execute(funcName, ...args);
+	}
+
 };

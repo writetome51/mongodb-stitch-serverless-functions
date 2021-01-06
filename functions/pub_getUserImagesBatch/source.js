7d6498d238b16b61dce1c3bf7ebd152bd52c1841
Steve Thompson <writetome51@gmail.com>
2021-01-06 14:46:51 -0700

    Minor changes to pub_getUserImagesBatch()

--- functions/pub_getUserImagesBatch/source.js
+++ functions/pub_getUserImagesBatch/source.js
@@ -1,15 +1,21 @@
 exports = async function({batchSize, batchNumber, sessionID}) {
-	let user = await context.functions.execute("getUser", {sessionID});
-	let imagesCollection = exec("getImagesCollection");
-	let imagesCursor = await imagesCollection.find({_user_id: user._id});
-	let dataTotal = await imagesCursor.count();
-	let images = imagesCursor.sort({_id: 1})
-		.skip((batchNumber - 1) * batchSize)
-		.limit(batchSize).toArray();
+	return await exec("handlePublicFunction",
+		async () => {
+			let imagesCollection = exec("getImagesCollection");
 
-	return await exec("getBatchOfImages",
-		images,
-		dataTotal
+			let user = await context.functions.execute("getUser", {sessionID});
+			let imagesCursor = await imagesCollection.find({_user_id: user._id});
+			var dataTotal = await imagesCursor.count();
+
+			var images = imagesCursor.sort({_id: 1})
+				.skip((batchNumber - 1) * batchSize)
+				.limit(batchSize).toArray();
+
+			return await exec("getBatchOfImages",
+				images,
+				dataTotal
+			);
+		}
 	);
 
 

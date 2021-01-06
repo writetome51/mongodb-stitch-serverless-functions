db3b77ceef8cd2783abdb6e908b308537745dd35
Steve Thompson <writetome51@gmail.com>
2021-01-05 20:37:40 -0700

    minor change

--- functions/pub_getUserImagesBatch/source.js
+++ functions/pub_getUserImagesBatch/source.js
@@ -1,7 +1,7 @@
 exports = async function({batchSize, batchNumber, sessionID}) {
-	var {_user_id} = await context.functions.execute("getUser", {sessionID});
+	let user = await context.functions.execute("getUser", {sessionID});
 	let imagesCollection = exec("getImagesCollection");
-	let imagesCursor = await imagesCollection.find({_user_id});
+	let imagesCursor = await imagesCollection.find({_user_id: user._id});
 	let dataTotal = await imagesCursor.count();
 	let images = imagesCursor.sort({_id: 1})
 		.skip((batchNumber - 1) * batchSize)

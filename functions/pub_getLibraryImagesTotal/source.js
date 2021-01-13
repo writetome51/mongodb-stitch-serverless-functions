04d2ca95b41549f94207cf82b14b3c1638f1b55e
Steve Thompson <writetome51@gmail.com>
2021-01-12 20:11:20 -0700

    minor changes

--- functions/pub_getLibraryImagesTotal/source.js
+++ functions/pub_getLibraryImagesTotal/source.js
@@ -5,8 +5,13 @@ exports = async function({name, sessionID}) {
 		async () => {
 			var user = await exec("getLoggedInUser", {sessionID});
 			var lib = await exec("getLibrary", {_user_id: user._id, name});
+			var _image_ids = convertedToArray(lib._image_ids)
+			return {dataTotal: _image_ids.length};
 
-			return {dataTotal: lib._image_ids.length};
+
+			function convertedToArray(arr) {
+				return [].concat(arr);
+			}
 		}
 	);
 };
